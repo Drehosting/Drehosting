2b9255962d2982bb7a165ae205a3eda6c9148c25
Drehosting <chris@dreher.co.uk>
2021-08-29 14:53:31 +0100

    Working.

--- backend.tf
+++ backend.tf
@@ -1,16 +1,12 @@
 terraform {
 
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  use_msi = true
-
+  
   backend "azurerm" {
   storage_account_name = "drehstg001" 
   container_name = "dretf001"
   key = "tf-DHMain.tfstate" 
+  subscription_id       = "447fdc4c-f9f4-4b14-ab7e-aa40f9745b9d"
+  tenant_id             = "e59d8da1-b9cc-4a55-a4cf-f08bc9fbe3ac"
+  resource_group_name   = "DRE-RSG-001"  
  }
-}
 }
