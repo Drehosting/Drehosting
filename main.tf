46eb2119d5baf1280c31313ff6ed297b8672acb5
Drehosting <chris@dreher.co.uk>
2021-08-29 14:23:02 +0100

    RemoveSAS Key

--- main.tf
+++ main.tf
@@ -2,5 +2,12 @@ resource "azurerm_resource_group" "resourcegroup" {
   name     = var.resourcename
   location = var.location
   tags     = var.tags
+}
 
+resource "azurerm_virtual_network" "virtualnetwork" {
+  name                = var.vnetname
+  address_space       = ["172.160.0.0/20"]
+  location            = var.location
+  resource_group_name = var.resourcename
+  tags                = var.tags
 }
