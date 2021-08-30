3b2fc14369b9b29d43c2d30f2eabeb0fdd620f60
Drehosting <chris@dreher.co.uk>
2021-08-30 12:36:03 +0100

    Yes

--- TF-DC-Build/main.tf
+++ TF-DC-Build/main.tf
@@ -1,1 +1,14 @@
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = var.resourcename
+  location = var.location
+  tags     = var.tags
+}
 
+resource "azurerm_virtual_network" "virtualnetwork" {
+  name                = var.vnetname
+  address_space       = ["172.16.0.0/20"]
+  location            = var.location
+  resource_group_name = var.resourcename
+  tags                = var.tags
+
+}
