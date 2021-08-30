a3b439c7eb49732685417947aa9c2126e75831e0
Drehosting <chris@dreher.co.uk>
2021-08-30 11:22:14 +0100

    yes

--- main.tf
+++ main.tf
@@ -6,8 +6,55 @@ resource "azurerm_resource_group" "resourcegroup" {
 
 resource "azurerm_virtual_network" "virtualnetwork" {
   name                = var.vnetname
-  address_space       = ["172.160.0.0/20"]
+  address_space       = ["172.16.0.0/20"]
   location            = var.location
   resource_group_name = var.resourcename
   tags                = var.tags
+
+ resource "azurerm_subnet" "GatewaySubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = var.resourcename
+  virtual_network_name = var.vnetname
+  address_prefixes     = ["172.16.0.0/24"]
+ }
+
+ resource "azurerm_subnet" "ProdSubnet" {
+  name                 = "ProdSubnet"
+  resource_group_name  = var.resourcename
+  virtual_network_name = var.vnetname
+  address_prefixes     = ["172.16.1.0/24"]
+ }
+
+  resource "azurerm_local_network_gateway" "onpremise" {
+  name                = "onpremise"
+  location            = var.location
+  resource_group_name = var.resourcename
+  gateway_address     = "168.62.225.23"
+  address_space       = ["10.1.1.0/24"]
 }
+
+resource "azurerm_public_ip" "AzureGWIP" {
+  name                = "AzureGWIP"
+  location            = var.location
+  resource_group_name = var.reourcename
+  allocation_method   = "Static"
+}
+
+resource "azurerm_virtual_network_gateway" "SitetoSite" {
+  name                = "SiteToSite"
+  location            = var.location
+  resource_group_name = var.resourcename
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "Basic"
+
+  ip_configuration {
+    public_ip_address_id          = azurerm_public_ip.AzureGWIP
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = subnet.VPNGWSubnet
+  }
+}
+ }
